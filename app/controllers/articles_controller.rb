cd62c8dfcaf0cf48ebe1ca43aa0161363cff9b43
plum870131 <R09522620@ntu.edu.tw>
2023-01-10 06:22:54 +0800

    form: save data to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,11 @@ class ArticlesController < ApplicationController
         # strong parameters protecting db
         # using params.require.permit to filter out trash
         @article = Article.new(params.require(:article).permit(:title, :descriptions))
-        render plain: @article
+        @article.save
+
+        # 結束動作後秀出該article
+        # redirect_to article_path(@article) #article_path = "/article" 而:id以@article自己extract id
+        redirect_to @article # 簡寫
     end
 
 end
