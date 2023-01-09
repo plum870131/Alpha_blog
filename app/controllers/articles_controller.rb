412c471a7b2b253a757e21f0bb271d9c68c30ec1
plum870131 <R09522620@ntu.edu.tw>
2023-01-10 06:08:56 +0800

    hand code article creation form w/o dealing w db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,15 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        # strong parameters protecting db
+        # using params.require.permit to filter out trash
+        @article = Article.new(params.require(:article).permit(:title, :descriptions))
+        render plain: @article
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
 
   # 利用resources 生成 CRUD actions 的 routes （table name plural）
-  resources :articles , only: [:show, :index] # 如果只要其中幾個action使用only
+  resources :articles , only: [:show, :index, :new, :create] # 如果只要其中幾個action使用only
+  # get "/articles/new", to: "articles#new"
+  # post "/articles", to: "articles#create"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
 end
