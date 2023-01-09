c5a19aefac4aed2d584bc6fbdaaf00d34526e23b
plum870131 <R09522620@ntu.edu.tw>
2023-01-09 19:52:39 +0800

    add articles table listing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
 
   # 利用resources 生成 CRUD actions 的 routes （table name plural）
-  resources :articles, only: [:show] # 如果只要其中幾個action使用only
+  resources :articles , only: [:show, :index] # 如果只要其中幾個action使用only
 end
