e18f4fb1c58ddad6eb9c1145fd134d6a6ab479d3
plum870131 <R09522620@ntu.edu.tw>
2023-01-10 06:48:26 +0800

    add error and notice message to new data action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,18 @@ class ArticlesController < ApplicationController
         # strong parameters protecting db
         # using params.require.permit to filter out trash
         @article = Article.new(params.require(:article).permit(:title, :descriptions))
-        @article.save
+        if @article.save
+            # 利用flash的:notice功能跳出提示視窗
+            # flash view template at app/vies/layouts/application
+            flash[:notice] = "Article saved."
 
-        # 結束動作後秀出該article
-        # redirect_to article_path(@article) #article_path = "/article" 而:id以@article自己extract id
-        redirect_to @article # 簡寫
+            # 結束動作後秀出該article
+            # redirect_to article_path(@article) #article_path = "/article" 而:id以@article自己extract id
+            redirect_to @article # 簡寫
+        else
+            render 'new' # render該controller內名為'new.html.erb'的模版
+        end
+
     end
 
 end
