1c51c13245f91ebecd2add4c25bdd1cc108a7e8b
plum870131 <R09522620@ntu.edu.tw>
2023-01-22 00:01:58 +0800

    downcase email before saved by adding callback:before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :articles
 
+    before_save { self.email = email.downcase }
+
     validates :name, presence: true, uniqueness: true, 
                 length: { minimum: 3, maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
